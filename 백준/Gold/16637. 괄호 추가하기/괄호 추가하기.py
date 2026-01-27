1c72b222ef5cd00d430a699857e4f5d5e7db8ad0
Yuje-Lee <65337423+dbwp031@users.noreply.github.com>
2026-01-27 17:08:27 +0900

    [Gold III] Title: 괄호 추가하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -1,55 +1,38 @@
 import sys
-import collections
+input = sys.stdin.readline
 
-def calc(a, exp, b):
-    a = int(a)
-    b = int(b)
-    if exp == "+":
-        num = a + b
-    elif exp == "-":
-        num = a - b
-    elif exp == "*":
-        num = a * b
-    return num
+def calc(a, op, b):
+    if op == "+": return a + b
+    if op == "-": return a - b
+    return a * b
 
-def calc_list(lst):
-    if len(lst) == 0:
-        print("ERROR")
+n = int(input().strip())
+s = input().strip()
+
+nums = list(map(int, s[0::2]))
+ops = list(s[1::2])
+
+if len(nums) == 1:
+    print(nums[0])
+    sys.exit(0)
+
+ans = -2**31
+
+# 계산 우선순위 없이 왼쪽부터 연산하다 보니까
+# dfs 해서 (왼쪽, 대상 ops, 오른쪽) 으로 처리할 수 있는 거임
+def dfs(i, res):
+    global ans
+    
+    if i == len(ops):
+        ans = max(ans, res)
         return
-    result = int(lst[0])
+    
+    # 1) 괄호 안치기
+    dfs(i + 1, calc(res, ops[i], nums[i + 1]))
+    # 2) 괄호 치기
+    if i + 1 < len(ops):
+        bracket  = calc(nums[i+1], ops[i+1], nums[i+2])
+        dfs(i+2, calc(res,ops[i], bracket))
 
-    for i in range(1, len(lst), 2):
-        # print(i)
-        exp = lst[i]
-        result = calc(result, exp, lst[i+1])
-    return result
-
-n = int(input())
-data = list(input())
-# print(data)
-ans = calc_list(data)
-for i in range(1, len(data), 2):
-    q = collections.deque()
-    q.append([i])
-    while q:
-        exp_indexes = set(q.popleft())
-        # print(exp_indexes)
-        transformed = []
-        for j in range(len(data)):
-            if j+1 in exp_indexes: # 괄호 exp면 계산하고 3개 -> 1개
-                exp = data[j+1]
-                num = calc(data[j], data[j+1], data[j+2])
-                transformed.append(num)
-            elif j in exp_indexes or j-1 in exp_indexes: # 
-                continue
-            else:
-                transformed.append(data[j])
-        # print(transformed)
-        result = calc_list(transformed)
-        # print(result)
-        ans = max(result, ans)
-
-        max_exp_index = max(exp_indexes)
-        for j in range(max_exp_index + 4, len(data), 2):
-            q.append(exp_indexes | {j,})
+dfs(0, nums[0])
 print(ans)

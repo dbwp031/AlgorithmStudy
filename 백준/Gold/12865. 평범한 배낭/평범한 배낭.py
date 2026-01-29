fdf701cbdd68ab52a57e5898849b64e1d8f874cd
Yuje-Lee <65337423+dbwp031@users.noreply.github.com>
2026-01-29 16:42:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 110328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,23 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 N,K = map(int,input().split())
-data = []
+
+dp = [0] * (K+1)
+
 for _ in range(N):
-    data.append(list(map(int,input().split())))
+    w, v = map(int,input().split())
 
-dp = [[0] * (K+1) for _ in range(N)]
+    # 최대 한도부터 물건 무게까지
+    for j in range(K,w-1,-1):
+            cand = dp[j-w] + v
+            if cand > dp[j]:
+                dp[j] = cand
 
-for i in range(data[0][0], K+1):
-    dp[0][i] = data[0][1]
-
-for i in range(1, N):
-    w = data[i][0]
-    v = data[i][1]
-    for j in range(1,K+1):
-        if j-w >= 0:
-            dp[i][j] = max(dp[i-1][j-w] + v, dp[i-1][j])
-        else:
-            dp[i][j] = dp[i-1][j]
-            
-print(max([max(l) for l in dp]))
-
-# for d in dp:
-#     print(d)
+print(dp[K])

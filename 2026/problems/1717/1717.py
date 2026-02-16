0492c91f6ad8bad3143c6f1a8edd0f1eb4958ef0
YUJE LEE <dbwp031@gmail.com>
2026-02-16 18:24:06 +0900

    update 260216

--- 2026/problems/1717/1717.py
+++ 2026/problems/1717/1717.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10_000)
+
 n,m = map(int,input().split())
 
 p = [-1] * (n+1)
@@ -7,6 +10,15 @@ def find(x):
         return x
     p[x] = find(p[x])
     return p[x]
+
+# def find(x):
+#     root = x
+#     while p[root] >= 0:
+#         root = p[root]
+
+#     while x != root:
+#         p[x], x = root, p[x]
+#     return root
 
 def union(u,v):
     u = find(u)

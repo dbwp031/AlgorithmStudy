acb02378b96c801dffe73158dbbb59eff0bced18
Yuje-Lee <65337423+dbwp031@users.noreply.github.com>
2026-02-24 21:52:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 3860 ms, Memory: 279768 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,17 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-N,K = map(int,input().split())
+n,k = map(int,input().split())
+ws = []
+vs = []
+for _ in range(n):
+    w,v = map(int,input().split())
+    ws.append(w)
+    vs.append(v)
+# i번째를 선택할지, 고려할지를 결정해야함.
+# 고려가 맞음.
 
-dp = [0] * (K+1)
+dp = [[0]*100_001 for _ in range(100)]
 
-for _ in range(N):
-    w, v = map(int,input().split())
+for i in range(ws[0], k+1):
+    dp[0][i] = vs[0]
 
-    # 최대 한도부터 물건 무게까지
-    for j in range(K,w-1,-1):
-            cand = dp[j-w] + v
-            if cand > dp[j]:
-                dp[j] = cand
-
-print(dp[K])
+for i in range(1, n):
+    w,v = ws[i], vs[i]
+    # dp[i][j] = i번째 원소까지 고려했을 때, j무게로 최대 value
+    for j in range(1,k+1):
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-w] + v,dp[i-1][j])
+# for d in dp[:n]:
+#     print(d[:k+1])
+print(dp[n-1][k])
